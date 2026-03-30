34b6ece8c58f719c18c4ab214a9687378f0b36d3
Priyanka Anand <anandpriyanka186@gmail.com>
2026-03-30 19:55:37 +0530

    connect DB to backend

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 
@@ -7,12 +9,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// DB connection
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
 // test route
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-// server start
 const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
